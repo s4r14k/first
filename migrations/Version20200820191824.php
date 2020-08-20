62b6ce605ec9b64ed5a6a0d7ea7b2cbbd91520d0
s4r14k <sarikraf@gmail.com>
2020-08-20 21:46:50 +0200

    test

---
+++ migrations/Version20200820191824.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200820191824 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE article ADD author VARCHAR(255) NOT NULL, DROP date, CHANGE id id INT AUTO_INCREMENT NOT NULL, CHANGE content content VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE article ADD date DATETIME NOT NULL, DROP author, CHANGE id id INT NOT NULL, CHANGE content content TEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
